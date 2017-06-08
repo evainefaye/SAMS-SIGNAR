d05a7da8c275314442ac3823d68d6840065df04d
Stephanie Martinez <stephanie@hawkbane.net>
2017-06-08 15:38:25 -0500

    Basic SignalR Server with default Hello Function Only

---
+++ SignalR.Server.Web/MyHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Server.Web
+{
+    public class MyHub : Hub
+    {
+
+        // Default Server Method
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ SignalR.Server.Web/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(SignalR.Server.Web.Startup))]
+
+namespace SignalR.Server.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            HubConfiguration hubConfiguration = new HubConfiguration();
+            hubConfiguration.EnableDetailedErrors = true;
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR(hubConfiguration);
+            GlobalHost.Configuration.MaxIncomingWebSocketMessageSize = null;
+        }
+    }
+}
