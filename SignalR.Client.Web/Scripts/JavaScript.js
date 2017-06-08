adf5e6c8c22757751bdcaa2f51467d693bd0d841
Stephanie Martinez <stephanie@hawkbane.net>
2017-06-08 16:21:11 -0500

    SignalR Client, Removed References as unnecessary

---
+++ SignalR.Client.Web/Scripts/JavaScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    $.connection.hub.url = "http://sams.hawkbane.biz/signalr/";
+    myHub = $.connection.myHub;
+
+    // Default Hello Method
+    myHub.client.hello = function () {
+    };
+
+    $.connection.hub.start()
+        .done(function () {
+            console.log("SignalR Server Connected Successfully");
+        })
+        .fail(function () {
+            console.log("SignalR Server Failed to Connect");
+        });
+});
